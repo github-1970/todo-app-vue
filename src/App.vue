99994cf13db092985795250f653e64c0c5b7dc66
github-1970 <github1970@gmail.com>
2023-11-24 20:53:46 +0330

    Start create template

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
 </template>
 
 <script setup>
-  import HomePage from './views/HomePage.vue'
+import HomePage from "./views/HomePage.vue";
 </script>
 
 <style>
-
+.glass {
+  background: rgba(255, 255, 255, 0.15);
+  /* backdrop-filter: blur(10px); */
+  border-radius: 10px;
+}
 </style>
